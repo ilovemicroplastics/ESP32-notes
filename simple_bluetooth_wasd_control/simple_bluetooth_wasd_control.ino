7f023d39c452330c0135f662377931531692c544
ilovemicroplastics <102258130+ilovemicroplastics@users.noreply.github.com>
2025-03-28 15:02:49 +0000

    Update simple_bluetooth_wasd_control.ino

--- simple_bluetooth_wasd_control/simple_bluetooth_wasd_control.ino
+++ simple_bluetooth_wasd_control/simple_bluetooth_wasd_control.ino
@@ -55,59 +55,51 @@ void loop() {
             for(int k = 0; k < 4; k++){
               setMotor(1, 255, PWM[k], INP_1[k], INP_2[k]);
             }
-            // Keep moving for 2 seconds
-            delay(2000);
-  
-            // Stop all motors
-            for(int k = 0; k < 4; k++){
-              setMotor(0, 0, PWM[k], INP_1[k], INP_2[k]);
-            }
             break;
         case 'a':
             // Turn left: Left motors backward, Right motors forward
             // Left motors (RB, LB) move backward, Right motors (RF, LF) move forward
             setMotor(1, 255, PWM[0], INP_1[0], INP_2[0]); // RB
-            setMotor(-1, 255, PWM[1], INP_1[1], INP_2[1]); // LB
+            setMotor(1, 150, PWM[1], INP_1[1], INP_2[1]); // LB  // reverse 
             setMotor(1, 255, PWM[2], INP_1[2], INP_2[2]);  // RF
-            setMotor(-1, 255, PWM[3], INP_1[3], INP_2[3]);  // LF
-            // Keep moving for 2 seconds
-            delay(2000);
-  
-            // Stop all motors
-            for(int k = 0; k < 4; k++){
-              setMotor(0, 0, PWM[k], INP_1[k], INP_2[k]);
-            }
+            setMotor(1, 150, PWM[3], INP_1[3], INP_2[3]);  // LF // reverse
+            break;
+        case 'aa':
+            // Turn left: Left motors backward, Right motors forward
+            // Left motors (RB, LB) move backward, Right motors (RF, LF) move forward
+            setMotor(1, 255, PWM[0], INP_1[0], INP_2[0]); // RB
+            setMotor(-1, 255, PWM[1], INP_1[1], INP_2[1]); // LB  // reverse 
+            setMotor(1, 255, PWM[2], INP_1[2], INP_2[2]);  // RF
+            setMotor(-1, 255, PWM[3], INP_1[3], INP_2[3]);  // LF // reverse
             break;
         case 's':
             // Move all motors backward
             for(int k = 0; k < 4; k++){
               setMotor(-1, 255, PWM[k], INP_1[k], INP_2[k]);
             }
-            // Keep moving for 2 seconds
-            delay(2000);
-  
-            // Stop all motors
-            for(int k = 0; k < 4; k++){
-              setMotor(0, 0, PWM[k], INP_1[k], INP_2[k]);
-            }
             break;
         case 'd':
             // Turn right: Left motors forward, Right motors backward
             // Left motors (RB, LB) move forward, Right motors (RF, LF) move backward
-            setMotor(-1, 255, PWM[0], INP_1[0], INP_2[0]); // RB
+            setMotor(1, 150, PWM[0], INP_1[0], INP_2[0]); // RB  // reverse
             setMotor(1, 255, PWM[1], INP_1[1], INP_2[1]); // LB
-            setMotor(-1, 255, PWM[2], INP_1[2], INP_2[2]); // RF
+            setMotor(1, 150, PWM[2], INP_1[2], INP_2[2]); // RF   // reverse
             setMotor(1, 255, PWM[3], INP_1[3], INP_2[3]); // LF
-            // Keep moving for 2 seconds
-            delay(2000);
-  
+            break;
+        case 'dd':
+            // Turn right: Left motors forward, Right motors backward
+            // Left motors (RB, LB) move forward, Right motors (RF, LF) move backward
+            setMotor(-1, 255, PWM[0], INP_1[0], INP_2[0]); // RB  // reverse
+            setMotor(1, 255, PWM[1], INP_1[1], INP_2[1]); // LB
+            setMotor(-1, 255, PWM[2], INP_1[2], INP_2[2]); // RF   // reverse
+            setMotor(1, 255, PWM[3], INP_1[3], INP_2[3]); // LF
+            break;    
+        default:
+            delay(1500);
             // Stop all motors
             for(int k = 0; k < 4; k++){
               setMotor(0, 0, PWM[k], INP_1[k], INP_2[k]);
             }
-            break;
-        default:
-            // Handle other characters, if needed
             break;
     }
   }
